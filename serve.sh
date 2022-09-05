d2c4041da34b8be65cde48cf5f59b984e5008d4a
Guansong Wang <wang.guansong@hotmail.com>
2022-09-05 11:14:09 +0800

    Add hugo server script.

---
+++ serve.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#/snap/hugo/current/bin/hugo server --bind=0.0.0.0 --baseURL=http://guansong.wang/
+hugo server --bind=0.0.0.0 --baseURL=http://guansong.wang/ --buildDrafts --verbose
+#hugo server --disableFastRender --i18n-warnings
